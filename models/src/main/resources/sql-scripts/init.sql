a2d029b2835a15a168304e0dd9d1367e08e7a203
Ariana Kržan <ak1193@student.uni-lj.si>
2023-12-05 13:12:04 +0100

    sql

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,3 +1,4 @@
 INSERT INTO lottery_drawing_results (drawing_date, winning_combination, lottery_category, total_prize) VALUES ('2023-01-15 20:00:00', '5,12,23,34,45', 'Regular', 100000.00);
 INSERT INTO lottery_drawing_results (drawing_date, winning_combination, lottery_category, total_prize) VALUES ('2023-02-15 20:00:00', '1,29,22,30,44', 'Regular', 140000.00);
 INSERT INTO lottery_drawing_results (drawing_date, winning_combination, lottery_category, total_prize) VALUES ('2023-03-15 20:00:00', '14,18,27,35,40', 'Regular', 180000.00);
+INSERT INTO lottery_drawing_results (drawing_date, winning_combination, lottery_category, total_prize) VALUES ('2023-03-15 20:00:00', '21,24,37,38,43', 'Regular', 180000.00);
