17c283687fd5379a6c4342845d1a585559b780b1
lb4684 <lb4684@student.uni-lj.si>
2023-12-23 22:48:57 +0100

    Kubernetes in ingress

--- api/src/main/java/si/fri/rso/simplsrecka/lotterydrawingresults/api/v1/LotteryDrawingResultsApplication.java
+++ api/src/main/java/si/fri/rso/simplsrecka/lotterydrawingresults/api/v1/LotteryDrawingResultsApplication.java
@@ -10,15 +10,11 @@ import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.info.License;
 import org.eclipse.microprofile.openapi.annotations.servers.Server;
 
-//@OpenAPIDefinition(info = @Info(title = "Lottery drawings and results API", version = "v1",
-//        contact = @Contact(email = "lb4684@student.uni-lj.si"),
-//        license = @License(name = "dev"), description = "API for managing image metadata."),
-//        servers = @Server(url = "http://localhost:8080/"))
 @OpenAPIDefinition(info = @Info(title = "Lottery drawings and results API", version = "v1",
         contact = @Contact(email = "lb4684@student.uni-lj.si"),
         license = @License(name = "dev"), description = "API for drawings and results."),
         //servers = @Server(url = "http://localhost:8083/"))
-        servers = @Server(url = "http://20.124.175.39:8083/"))
+        servers = @Server(url = "http://52.226.192.46/lottery-drawing-results"))
 @ApplicationPath("/v1")
 public class LotteryDrawingResultsApplication extends Application {
 
